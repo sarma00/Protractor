98e9e0d5f39e4f5a56e653f0b2ddd07c6630fad6
Manash Sarma <Manash.Sarma@amdocs.com>
2021-02-12 12:48:13 +0530

    added other flows for calculator

--- tests/CalculatorPOMTest.js
+++ tests/CalculatorPOMTest.js
@@ -18,5 +18,21 @@ describe('calculator pom demo',function(){
         homepage.verifyResults('20');
         browser.sleep(2000);        
     });
+    it('multiplication test', function(){
+        homepage.enterFirstNumber('10');
+        homepage.enterSecondNumber('5');
+        homepage.selectOperator('divide');
+        homepage.clickGoBtn();
+        homepage.verifyResults('50');
+        browser.sleep(2000);        
+    });
+    it('substraction test', function(){
+        homepage.enterFirstNumber('10');
+        homepage.enterSecondNumber('5');
+        homepage.selectOperator('divide');
+        homepage.clickGoBtn();
+        homepage.verifyResults('5');
+        browser.sleep(2000);        
+    });
 
 });
